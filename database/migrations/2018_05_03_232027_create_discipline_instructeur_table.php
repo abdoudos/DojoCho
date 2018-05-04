61d833b994d1e595604554048d0bfd13631fc067
kamikaze <benaliaabdelouhab@gmail.com>
2018-05-04 11:04:03 +0100

    creer la relation discipline_instructeur

--- database/migrations/2018_05_03_232027_create_discipline_instructeur_table.php
+++ database/migrations/2018_05_03_232027_create_discipline_instructeur_table.php
@@ -16,6 +16,12 @@ class CreateDisciplineInstructeurTable extends Migration
         Schema::create('discipline_instructeur', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
+            // lien vers la discipline
+            $table->integer('discipline_id')->unsigned();
+            $table->foreign('discipline_id')->references('id')->on('disciplines');
+            //lien vers l'instructeur
+            $table->integer('instructeur_id')->unsigned();
+            $table->foreign('instructeur_id')->references('id')->on('instructeurs');
         });
     }
 
